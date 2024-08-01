f0e054c5b643f8107f6aaea3a3be9625f9ca8131
nathxrz <nathyrezendemachado@gmail.com>
2024-08-01 16:49:37 -0300

    Sprite + background

--- src/init.js
+++ src/init.js
@@ -10,16 +10,16 @@ let bgPattern=null
 let x = 0
 let y = 0
 
-let cellWidth = 165		//largura da celular de recorte
-let cellHeight = 177	//altura da celula de recorte
-let totalSprites = 3	//Total de sprites
+let cellWidth = 32		//largura da celular de recorte
+let cellHeight = 50	//altura da celula de recorte
+let totalSprites = 4	//Total de sprites
 let goblinSpeed =  5 	//Velocidade de troca de sprites (anime)
 
 const init = async () => {
 	console.log("Initialize Canvas")
 	CANVAS = document.querySelector('canvas')
 	CTX = CANVAS.getContext('2d')
-	goblinImage = await loadImage('img/goblin.png')
+	goblinImage = await loadImage('img/skeleton.png')
 	bgImage = await loadImage('img/background-gray.png')
 	bgPattern = CTX.createPattern(bgImage,'repeat')
 	loop()
@@ -29,7 +29,7 @@ const init = async () => {
 const animeSprite = (spriteSpeed)=>{ //Controla a animacao do sprite
 	setInterval(() => {
 		x = x < totalSprites - 1 ? x + 1 : 0;
-	}, 1000 / (FRAMES*spriteSpeed/10))
+	}, 1000 / (FRAMES*spriteSpeed/100))
 }
 
 const loop = () => {
@@ -44,7 +44,7 @@ const loop = () => {
 			y,
 			cellWidth,
 			cellHeight, //source
-			100, 10, 165, 174 //draw
+			100, 10, 32, 50 //draw
 		)
 
 		requestAnimationFrame(loop)
